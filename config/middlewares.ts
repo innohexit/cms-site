051ae5924a9bf83d26dd87f6e788c9ea15e4755d
Nitesh000 <ntudu040@gmail.com>
2024-08-11 22:04:25 +0530

    feat: strapi db setup

---
+++ config/middlewares.ts
@@ -0,0 +1,11 @@
+module.exports = ({ env }) => [
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::logger",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+];
